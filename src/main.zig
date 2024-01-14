24c03fbd58c6e5f7bcb18d37233c2948462ed0cd
Kaio Delphino <github@kaiodelphino.com>
2024-01-14 11:27:41 -0300

    initial commit

---
+++ src/main.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+const rl = @cImport({
+    @cInclude("raylib.h");
+});
+
+pub fn main() !void {
+    // rl.SetConfigFlags(rl.ConfigFlags{ .FLAG_WINDOW_RESIZABLE = true });
+    rl.InitWindow(800, 800, "hello world!");
+    rl.SetTargetFPS(60);
+
+    defer rl.CloseWindow();
+
+    while (!rl.WindowShouldClose()) {
+        rl.BeginDrawing();
+        defer rl.EndDrawing();
+
+        rl.ClearBackground(rl.BLACK);
+        rl.DrawFPS(10, 10);
+
+        rl.DrawText("hello world!", 100, 100, 20, rl.YELLOW);
+    }
+}
+
+test "simple test" {}
